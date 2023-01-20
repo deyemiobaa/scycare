1d730c3c0e98af7f8872b84c2989966d77423b4a
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2023-01-20 18:00:14 +0100

    tailwind custom config

--- src/App.tsx
+++ src/App.tsx
@@ -2,5 +2,9 @@ import React from "react"
 import { cn } from "./utils"
 
 export default function App(): JSX.Element {
-	return <div className={cn("bg-white")} />
+	return (
+		<main className={cn("bg-accent-two w-full min-h-screen")}>
+			<div className={cn("mx-auto w-full max-w-md")}></div>
+		</main>
+	)
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,32 @@
 module.exports = {
 	content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: "#002C3F",
+				secondary: "#00C2E7",
+				tertiary: "#FF7658",
+				accent: {
+					one: "#80959F",
+					two: "#E0F6FC",
+					three: "#FFE7E2",
+				},
+				grey: {
+					one: "#595D6E",
+					two: "#656973",
+					three: "#828282",
+					four: "#74788D",
+					five: "#F2F3F8",
+				},
+			},
+			backgroundColor: {
+				button: {
+					hover: "linear-gradient(0deg, #0088A2, #0088A2), #092C4C;",
+					active: "linear-gradient(0deg, #0088A2, #0088A2), #092C4C;",
+					disabled: "#E0E6E8",
+				},
+			},
+		},
 	},
 	plugins: [],
 }
