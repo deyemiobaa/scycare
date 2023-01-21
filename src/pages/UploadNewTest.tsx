39b9b4d98c73de65e6a9b78d0ef79fb1e194a96c
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2023-01-21 22:25:02 +0100

    top section; upload new test

--- src/pages/UploadNewTest.tsx
+++ src/pages/UploadNewTest.tsx
@@ -1,5 +1,24 @@
 import React from "react"
+import Header from "../components/Header"
+import { cn } from "../utils"
+import { Link } from "react-router-dom"
 
 export default function UploadNewTest(): JSX.Element {
-	return <div>UploadNewTest</div>
+	return (
+		<div className={cn("bg-white")}>
+			<Header />
+			<div className={cn("px-5")}>
+				<div className={cn("flex items-center justify-between")}>
+					<h1 className={cn("text-2xl font-bold capitalize text-secondary")}>
+						Book new lab test
+					</h1>
+					<Link
+						to="/tracking"
+						className={cn("text-sm text-primary underline")}>
+						Track your test &gt;
+					</Link>
+				</div>
+			</div>
+		</div>
+	)
 }
