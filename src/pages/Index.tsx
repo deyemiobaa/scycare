50f720983ade7c9728b6dca92b722138b6fc2d63
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2023-01-21 01:39:37 +0100

    scycare landing page

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,5 +1,25 @@
 import React from "react"
+import { cn } from "../utils"
+import Button from "../components/Button"
 
 export default function Index(): JSX.Element {
-	return <div>Index</div>
+	return (
+		<div className={cn("h-screen bg-home bg-cover bg-center")}>
+			<div
+				className={cn(
+					"flex h-full flex-col items-center justify-center gap-4 bg-primary/[.85] px-5"
+				)}>
+				<h1 className={cn("text-center font-hellix text-4xl text-secondary")}>
+					Welcome to Scycare
+				</h1>
+				<p className={cn("mb-5 text-center")}>
+					Request, manage, and track your medical tests in simple steps
+				</p>
+				<Button
+					text="Get Started"
+					type="button"
+				/>
+			</div>
+		</div>
+	)
 }
