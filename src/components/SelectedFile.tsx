ed92b0c706e938ce6155923a1c324ff7464d28fc
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2023-02-11 23:39:28 +0100

    add  component to show selected file

---
+++ src/components/SelectedFile.tsx
@@ -0,0 +1,45 @@
+import React from "react"
+import { BsArrowDownShort } from "react-icons/bs"
+import { VscTrash } from "react-icons/vsc"
+
+interface Props {
+	fileName: string
+	fileSize: string
+	date: string
+}
+
+export default function SelectedFile(props: Props): JSX.Element {
+	const { fileName, fileSize, date } = props
+	return (
+		<div className="mb-6">
+			<div className="my-4 flex items-center gap-2">
+				<label
+					htmlFor="requestForm"
+					className="text-xs font-semibold text-gray-600">
+					Uploaded Lab Request
+				</label>
+				<BsArrowDownShort />
+			</div>
+			<div className="flex w-full items-center gap-3 rounded-md bg-white/80 p-4 shadow-min">
+				<img
+					src="/assets/icons/file_upload.png"
+					alt="upload file"
+					className="grayscale"
+				/>
+				<div>
+					<p className="mb-1 text-xs">{fileName}</p>
+					<p className="flex gap-3 text-xs">
+						<span>{date}</span>
+						<span>{fileSize}</span>
+					</p>
+				</div>
+				<button
+					type="button"
+					className="ml-auto"
+					title="remove file">
+					<VscTrash />
+				</button>
+			</div>
+		</div>
+	)
+}
